cb94951cb3631bb5ad91a601a44d5a255a9372dd
Appinventiv <sanju.bhatt@appinventiv.com>
2017-02-28 16:13:31 +0530

    no message

--- loginToFBK/ViewController.swift
+++ loginToFBK/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 import FBSDKLoginKit
+import FBSDKShareKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var images: UIImageView!
+    
+    @IBOutlet weak var addImage: UIButton!
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
         
         if(FBSDKAccessToken.current() == nil)
         {
@@ -34,6 +39,11 @@ class ViewController: UIViewController {
         loginButton.delegate = self
         self.view.addSubview(loginButton)
         
+        let likeButton = FBSDKLikeButton()
+        likeButton.frame = CGRect(x: 80, y: 90, width: 100, height: 100)
+        likeButton.objectID = "https://www.facebook.com/FacebookDevelopers"
+        self.view.addSubview(likeButton)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,8 +53,44 @@ class ViewController: UIViewController {
     
     }
     
+    
+    
+    @IBAction func addImages(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)
+        picker.delegate = self
 
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+
+        let newContent = FBSDKSharePhotoContent()
+        let photo = FBSDKSharePhoto()
+        let image : UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photo.image = image
+        photo.isUserGenerated = true
+        
+        newContent.photos = [photo]
+        images.image = image
+        dismiss(animated: true, completion: nil)
+        
+    
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+       
+        
+    }
+    
 }
+
+
+
 extension ViewController : FBSDKLoginButtonDelegate{
     
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!){
